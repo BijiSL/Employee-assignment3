d775f1059b2d4f38a913d1d8ffbafa014d1e12d3
BijiSL <bijisl91@gmail.com>
2024-10-13 22:50:05 +0530

    edit

--- app.js
+++ app.js
@@ -2,10 +2,7 @@
 var express=require("express");
 var morgan=require("morgan");
 require("dotenv").config();
-const employeeRoute=require('./routes/employeeRoute');
-// Task2: create mongoDB connection 
-
-require("./db/dbConnection");
+const employeeRoute=require('employeeRoute');
 var app=express();
 app.use(morgan("dev"));
 app.use(express.json());
@@ -20,13 +17,38 @@ app.use(express.static(path.join(__dirname+'/dist/FrontEnd')));
 
 
 
+//  Task2: create mongoDB connection 
 
+const mongoose=require("mongoose");
+const employeeModel = require("./model/employeeModel");
+mongoose
+.connect(process.env.mongo_url)
+.then(() => {
+    console.log("MongoDB Atlas is connected Successfully!!!");
+})
+.catch((err) => {
+    console.log(err);
+})
 
-
+const employeeSchema=mongoose.Schema({
+    employeeName:String,
+    employeeLocation:String,
+    employeeSalary:Number,
+    employeePosition:String
+    
+});
 
 
 //TODO: get data from db  using api '/api/employeelist'
-
+app.get('/api/employeelist',async(req,res)=>{
+    try{
+        var data=await employeeModel.find();
+       
+        res.status(200).send(data);
+    }catch(error){
+        res.status(404).send("unable to send data");
+    }
+});
 
 
 
@@ -34,6 +56,15 @@ app.use(express.static(path.join(__dirname+'/dist/FrontEnd')));
 
 
 
+app.get('/api/employeelist/:id',async(req,res)=>{
+    try{
+      var data= await employeeModel.findById(req.params.id);
+        res.status(200).send(data);
+    }catch(error){
+        res.status(404).send("unable to send data");
+    }
+    
+});
 
 
 //TODO: send data from db using api '/api/employeelist'
@@ -42,16 +73,44 @@ app.use(express.static(path.join(__dirname+'/dist/FrontEnd')));
 
 
 
+app.post('/api/employeelist',async(req,res)=>{
+    try{
+        var item=req.body;
+        var data=new employeeModel(item);
+        await data.save();
+        data=JSON.stringify(await employeeModel.find(),null,2);
+        res.status(200).send(`Data added successfully!!\n${data}`);
 
+    }catch(error){
+     res.status(404).send("unable to send data");
+    }
+});
+// const employeeModel=mongoose.model('employee',employeeSchema);
 
 //TODO: delete a employee data from db by using api '/api/employeelist/:id'
-
-
+app.delete('/api/employeelist/:id',async(req,res)=>{
+    try{
+        await employeeModel.findByIdAndDelete(req.params.id);
+        res.status(200).send("Deleted Successfully!!");
+    }catch(error){
+        res.status(404).send("unable to send data");
+    }
+    
+});
 
 
 
 //TODO: Update  a employee data from db by using api '/api/employeelist'
 //Request body format:{name:'',location:'',position:'',salary:''}
+app.put('/api/employeelist',async(req,res)=>{
+    try{
+        await employeeModel.findByIdAndUpdate(req.params.id,req.body);
+        res.status(200).send("Updated successfully!!!");
+    }catch(error){
+res.status(404).send("Unable to send data");
+    }
+});
+
 
 
 //! dont delete this code. it connects the front end file.
